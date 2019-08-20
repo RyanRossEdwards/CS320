59006de44602858b5ffb953b78575bbe18b72a39
Ryan Edwards <37044278+rryanedwards@users.noreply.github.com>
2019-08-20 13:26:56 +1200

    a2 edits

--- A2/a2.py
+++ A2/a2.py
@@ -1,5 +1,6 @@
 import sys
 from heapq import heappush, heappop, heapify
+from math import ceil
 
 class XNode:
     def __init__(self, x, y):
@@ -30,18 +31,67 @@ def main():
     # Preprocessing step in O(nlogn)
     p_x = []
     p_y = []
+    length = 0
 
     for line in sys.stdin:
         array = ([int(x) for x in line.split()])
         heappush(p_x, XNode(array[0], array[1]))
         heappush(p_y, YNode(array[0], array[1]))
+        length += 1
 
-    for i in range(len(p_x)):
-        node = heappop(p_x)
-        print(node.x, node.y)
-    for i in range(len(p_y)):
-        node = heappop(p_y)
-        print(node.x, node.y)
+    # for i in range(len(p_x)):
+    #     node = heappop(p_x)
+    #     print(node.x, node.y)
+    # for i in range(len(p_y)):
+    #     node = heappop(p_y)
+    #     print(node.x, node.y)
+
+
+# def ClosestPair(my_container):
+def ClosestPair(p_x, p_y, length):
+    # Compute vertical line L
+    l_median = ceil(length / 2)
+    
+    # add to U and V
+    u_x = []
+    u_y = []
+    v_x = []
+    v_y = []
+
+    for i in range(length):
+        if i < l_median:
+            # Add to U
+            # Length is l_median
+            p_x.
+        else:
+            # Add to V
+            # Length is length = l_median
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 main()
