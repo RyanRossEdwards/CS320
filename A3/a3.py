6261523e58a85c2f761581edb473a75c8680a7f6
Ryan Edwards <37044278+rryanedwards@users.noreply.github.com>
2019-09-11 01:06:58 +1200

    problem 3

--- A3/a3.py
+++ A3/a3.py
@@ -16,7 +16,7 @@ def main():
         # array = ([int(x) for x in line.split()])
         # print(array)
         
-        # Manually receiving the input for time efficiency
+        # Manually processing the input for time efficiency
         array = []
         i = 0
         try:
@@ -45,10 +45,48 @@ def main():
 
         # This means it's at the end of the line
         except IndexError:
+            # Last needs to be added
+            array += [[int(sn), int(fn)]]
+
             print(array)
+            start_first = sorted(array, key=lambda x: x[0])
+            end_first = sorted(array, key=lambda x: x[1])
+            print(start_first)
+            print(end_first)
+
+            print('Problem 3:')
+            problem3(start_first)
+            print()
 
 
+def problem3(array):
+    max_union = 0
+    new_union = True
+    union = 0
 
+    for i in range(len(array)):
+        sn, fn = array[i][0], array[i][1]
+        if new_union:
+            su, fu = sn, fn
+            new_union = False
+        # Overlap
+        elif sn <= fu and fn > fu:
+            fu = fn
+        # No Overlap
+        elif sn > fu:
+            union = fu - su
+            if union > max_union:
+                max_union = union
+            su, fu = sn, fn
+
+        if i == len(array)-1:
+            union = fu - su
+            if union > max_union:
+                max_union = union
+
+        print(i, '-', sn, fn, su, fu, union)
+
+    print(max_union)
 
 
 
