cfa708d255e802ff1ec1b4e8d15df21f356cea0f
Ryan Edwards <37044278+rryanedwards@users.noreply.github.com>
2019-09-22 03:08:31 +1200

    cashier algorithm

---
+++ Study/cashier.py
@@ -0,0 +1,63 @@
+def main():
+    print("Enter Amount in dollars $ (0 to exit)")
+
+    try:
+        amount = int(float(input()) * 100)
+
+        if amount == 0:
+            return False
+        
+        change = cashier(amount)
+
+        if type(change) == list:
+            change = [(lambda x: x/100)(i) for i in cashier(amount)]
+
+        print(change)
+
+    except ValueError:
+        print("Invalid input")
+
+    return True
+
+
+def cashier(amount):
+    denominations = [10,20,50,100,200,500,1000,2000,5000,10000]
+    change = []
+
+    try:
+        while amount > 0:
+            largest_coin = None
+
+            for coin in denominations:
+                if coin <= amount:
+                    largest_coin = coin
+                elif largest_coin == None:
+                    raise ValueError
+                else:
+                    break
+
+            amount = amount - largest_coin
+            change += [largest_coin]
+
+    except ValueError:
+        change = "No Exact Change"
+
+    return change
+
+run = True
+while run:
+    run = main()
+
+
+
+# Some Notes:
+    # There is an issue with handling floats
+    # run 1.3 - 1.0 and get 0.300000...4???
+
+    # change = cashier(amount)
+    # lambda notes:
+    # >>> a = lambda x: x*2
+    # >>> b = [1,2,3]
+    # >>> [a(i) for i in b]
+    # [2, 4, 6]
+    # >>> [(lambda x: x*2)(i) for i in b]
