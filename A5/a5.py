0918da27966ec7ebb7fc2d8b124375f12ca5a6d7
Ryan Edwards <37044278+rryanedwards@users.noreply.github.com>
2019-10-12 17:38:54 +1300

    assignment 5

---
+++ A5/a5.py
@@ -0,0 +1,96 @@
+import sys
+
+def main():
+    for line in sys.stdin:
+        # array = ([int(x) for x in line.split()])
+
+        # Manually splitting for efficiency
+        array = []
+        dp1 = []
+        dp2 = []
+        jewels_value = 0
+        number_string = ''
+
+        for char in line:
+
+            if char == ' ' or char == '\n':
+                number = int(number_string)
+                array += [number]
+                dp1 += [number]
+                dp2 += [number]
+
+                jewels_value += number
+
+                number_string = ''
+
+            else:
+                number_string += char
+
+        kings_daughter1 = maxSum(array, dp1, dp2)
+        kings_daughter2 = jewels_value - kings_daughter1
+
+        print(kings_daughter1, kings_daughter2)
+
+
+# To self - using built in max to find max, if it's slow manually implement it
+# To self - can combine the for loops for even greater efficiency
+
+def maxSum(array, dp1, dp2):
+
+    for i in range(2, len(array)-1):
+
+        for j in range(0, i-1):
+
+            if dp1[i] < dp1[j] + array[i]:
+                dp1[i] = dp1[j] + array[i]
+
+
+    for i in range(3, len(array)):
+
+        for j in range(0, i-1):
+
+            if dp2[i] < dp2[j] + array[i]:
+                dp2[i] = dp2[j] + array[i]
+
+
+    return max(max(dp1), max(dp2))
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+main()
